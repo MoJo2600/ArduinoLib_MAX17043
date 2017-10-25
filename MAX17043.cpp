a5af1bd70d0b2560df45a42aa0ec3aee84a819a0
Christian Erhardt <mail@mojo2k.de>
2017-10-25 21:41:13 +0200

    added sleep method

--- MAX17043.cpp
+++ MAX17043.cpp
@@ -87,6 +87,15 @@ void MAX17043::reset() {
 	writeRegister(COMMAND_REGISTER, 0x00, 0x54);
 }
 
+void MAX17043::sleep() {
+	byte MSB = 0;
+	byte LSB = 0;
+	
+	readConfigRegister(MSB, LSB);	
+	
+	writeRegister(CONFIG_REGISTER, MSB, LSB | 0x80);		
+}
+
 void MAX17043::quickStart() {
 	
 	writeRegister(MODE_REGISTER, 0x40, 0x00);
